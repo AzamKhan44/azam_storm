92bebe34a754b3a653d1ac4f7bd68fd5fccddc54
Mohd. Azam Khan <mohd.azam.khan@impetus.co.in>
2020-08-19 15:40:31 +0530

    Initial commit

---
+++ src/main/java/com/azam/storm/azam_storm/App.java
@@ -0,0 +1,13 @@
+package com.azam.storm.azam_storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
